107f2c93a55d35d0fe2aae5af14a9af3959326cd
Harris, Austin <harrisja@ornl.gov>
2026-03-24 09:04:47 -0400

    Add mismatch repair coverage for issue 30

--- tests/test_support.F90
+++ tests/test_support.F90
@@ -6,15 +6,22 @@ module test_support
    private
 
    public :: attach_mock_clock
+   public :: attach_scripted_mock_clock
    public :: build_stack
    public :: fake_time
+   public :: get_mock_clock_call_count
    public :: find_context_idx
    public :: mock_clock
    public :: read_file_text
+   public :: reset_mock_clock_state
    public :: read_unit_text
    public :: snapshot_timer
 
    real(wp), save :: fake_time = 0.0_wp
+   real(wp), allocatable, save :: scripted_times(:)
+   integer, save :: mock_clock_call_count = 0
+   integer, save :: scripted_time_idx = 0
+   logical, save :: use_scripted_times = .false.
 
 contains
 
@@ -22,8 +29,19 @@ contains
       class(ftimer_t), intent(inout) :: timer
 
       timer%clock => mock_clock
-      fake_time = 0.0_wp
+      call reset_mock_clock_state()
    end subroutine attach_mock_clock
+
+   subroutine attach_scripted_mock_clock(timer, values)
+      class(ftimer_t), intent(inout) :: timer
+      real(wp), intent(in) :: values(:)
+
+      timer%clock => mock_clock
+      call reset_mock_clock_state()
+      use_scripted_times = .true.
+      allocate (scripted_times(size(values)))
+      scripted_times = values
+   end subroutine attach_scripted_mock_clock
 
    type(ftimer_call_stack_t) function build_stack(ids) result(stack)
       integer, intent(in) :: ids(:)
@@ -45,8 +63,31 @@ contains
    function mock_clock() result(t)
       real(wp) :: t
 
+      mock_clock_call_count = mock_clock_call_count + 1
+
+      if (use_scripted_times) then
+         if (.not. allocated(scripted_times)) error stop "scripted mock clock is not configured"
+         if (scripted_time_idx >= size(scripted_times)) error stop "scripted mock clock exhausted"
+
+         scripted_time_idx = scripted_time_idx + 1
+         t = scripted_times(scripted_time_idx)
+         return
+      end if
+
       t = fake_time
    end function mock_clock
+
+   integer function get_mock_clock_call_count() result(count)
+      count = mock_clock_call_count
+   end function get_mock_clock_call_count
+
+   subroutine reset_mock_clock_state()
+      fake_time = 0.0_wp
+      mock_clock_call_count = 0
+      scripted_time_idx = 0
+      use_scripted_times = .false.
+      if (allocated(scripted_times)) deallocate (scripted_times)
+   end subroutine reset_mock_clock_state
 
    subroutine snapshot_timer(timer, state)
       class(ftimer_t), intent(in) :: timer
