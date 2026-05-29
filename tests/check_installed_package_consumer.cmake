aba0f7c93d4955b00d425ea05789c2a3d0ac516a
Harris, Austin <harrisja@ornl.gov>
2026-05-29 14:35:21 -0400

    Strengthen smoke evidence for installed consumers

--- tests/check_installed_package_consumer.cmake
+++ tests/check_installed_package_consumer.cmake
@@ -459,12 +459,19 @@ if(NOT consumer_build_result EQUAL 0)
 endif()
 
 set(consumer_executable "${consumer_build_dir}/ftimer_installed_consumer${TEST_EXECUTABLE_SUFFIX}")
+set(oop_consumer_executable "${consumer_build_dir}/ftimer_installed_oop_consumer${TEST_EXECUTABLE_SUFFIX}")
 if(DEFINED TEST_CONFIG AND NOT TEST_CONFIG STREQUAL "")
   set(configured_consumer_executable
     "${consumer_build_dir}/${TEST_CONFIG}/ftimer_installed_consumer${TEST_EXECUTABLE_SUFFIX}"
   )
+  set(configured_oop_consumer_executable
+    "${consumer_build_dir}/${TEST_CONFIG}/ftimer_installed_oop_consumer${TEST_EXECUTABLE_SUFFIX}"
+  )
   if(EXISTS "${configured_consumer_executable}")
     set(consumer_executable "${configured_consumer_executable}")
+  endif()
+  if(EXISTS "${configured_oop_consumer_executable}")
+    set(oop_consumer_executable "${configured_oop_consumer_executable}")
   endif()
 endif()
 
@@ -475,6 +482,15 @@ execute_process(
 )
 if(NOT consumer_run_result EQUAL 0)
   message(FATAL_ERROR "Installed-package consumer executable exited with a nonzero status.")
+endif()
+
+execute_process(
+  COMMAND "${oop_consumer_executable}"
+  WORKING_DIRECTORY "${consumer_build_dir}"
+  RESULT_VARIABLE oop_consumer_run_result
+)
+if(NOT oop_consumer_run_result EQUAL 0)
+  message(FATAL_ERROR "Installed-package OOP consumer executable exited with a nonzero status.")
 endif()
 
 if(TEST_ENABLE_MPI)
